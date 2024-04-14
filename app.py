3e5a641f8b9baa26e2668bd625e4d7ef68eed320
Let1fer <alekus2004@gmail.com>
2024-04-14 03:07:29 +0300

    Добавить .venv в .gitignore и отправить приветственное сообщение в Telegram

---
+++ app.py
@@ -0,0 +1,9 @@
+import requests
+import dotenv
+import os
+dotenv.load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+chat_id = os.getenv("CHAT_ID")
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+requests.post(url, data={"chat_id": chat_id, "text": "Hello, World!"})

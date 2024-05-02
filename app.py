ae0e142de351ffbdc61c6d13bb85ce33067fa0d8
Let1fer <alekus2004@gmail.com>
2024-05-03 01:05:40 +0300

    Добавить .venv в .gitignore и добавить новые файлы в репозиторий

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ TOKEN = os.getenv("TOKEN")
 chat_id = os.getenv("CHAT_ID")
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 requests.post(url, data={"chat_id": chat_id, "text": "Hello, World!"})
+print("Message sent!")
+print('Finished!')
